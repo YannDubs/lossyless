d06403da18dddf08ca181504e1cd234fd963e346
YannDubs <yanndubois96@gmail.com>
2021-01-25 11:06:10 -0500

    [BIN] mv dev.

--- bin/dev/dev_dist.sh
+++ bin/dev/dev_dist.sh
@@ -1,33 +1,36 @@
 #!/usr/bin/env bash
 
-experiment="imgs" # should always be called with -m dev
+experiment="dist" # should always be called with -m dev
 notes="
-**Goal**: Checking that all models on images run without errors (in parallel)
+**Goal**: Checking that all models on distributions run without errors (in parallel)
 **Hypothesis**: No errors
 "
 
-source `dirname $0`/utils.sh
+source `dirname $0`/../utils.sh
 
 # define all the arguments modified or added to `conf`. If they are added use `+`
 kwargs="
 experiment=$experiment 
 +mode=dev
+logger.name=false
 timeout=$time
+data=bananaRot
 $add_kwargs
 "
 
 # every arguments that you are sweeping over
 kwargs_multi="
-encoder=cnn,mlp,resnet
-distortion=ivib,ivae,ince,taskvib,vae,vib,nce
-rate=H_factorized,H_hyper,MI_unitgaussian,MI_vamp
-data=mnist
-" 
+encoder=mlp
+distortion=ivib,ivae,ince
+rate=H_factorized
+"
+
+
 
 if [ "$is_plot_only" = false ] ; then
   for kwargs_dep in  ""
   do
-
+    
     python "$main" +hydra.job.env_set.WANDB_NOTES="\"${notes}\"" $kwargs $kwargs_multi $kwargs_dep -m 
     
   done
--- bin/dev/dev_imgs.sh
+++ bin/dev/dev_imgs.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env bash
 
-experiment="dist" # should always be called with -m dev
+experiment="imgs" # should always be called with -m dev
 notes="
-**Goal**: Checking that all models on distributions run without errors (in parallel)
+**Goal**: Checking that all models on images run without errors (in parallel)
 **Hypothesis**: No errors
 "
 
-source `dirname $0`/utils.sh
+source `dirname $0`/../utils.sh
 
 # define all the arguments modified or added to `conf`. If they are added use `+`
 kwargs="
 experiment=$experiment 
 +mode=dev
+logger.name=false
 timeout=$time
-data=bananaRot
 $add_kwargs
 "
 
 # every arguments that you are sweeping over
 kwargs_multi="
-encoder=mlp
-distortion=ivib,ivae,ince
-rate=H_factorized
-"
+encoder=cnn,mlp,resnet
+distortion=ivib,ivae,ince,taskvib,vae,vib,nce
+rate=H_factorized,H_hyper,MI_unitgaussian,MI_vamp
+data=mnist
+" 
 
 if [ "$is_plot_only" = false ] ; then
   for kwargs_dep in  ""
   do
-    
+
     python "$main" +hydra.job.env_set.WANDB_NOTES="\"${notes}\"" $kwargs $kwargs_multi $kwargs_dep -m 
     
   done
