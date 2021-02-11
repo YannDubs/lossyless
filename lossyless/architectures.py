[BUG] galaxy in aggreagator: cnn and normalization

lossyless/architectures.py
@@ -245,7 +245,7 @@ class CNN(nn.Module):
 
     Notes
     -----
-    - Only works for images whose width and height are power of 2.
+    - Only works for images whose side is a power of 2 (not necessarily  squared).
     - If `in_shape` and `out_dim` are reversed (i.e. `in_shape` is int) then will transpose the CNN.
 
     Parameters
@@ -267,6 +267,10 @@ class CNN(nn.Module):
     activation : {"gdn"}U{any torch.nn activation}, optional
         Activation to use.
 
+    n_layers : int, optional, optional
+        Number of layers. If `None` uses the required number of layers so that the smallest side 
+        is 2 after encoding (i.e. one less than the maximum).
+
     kwargs :
         Additional arguments to `ConvBlock`.
     """
@@ -278,6 +282,7 @@ class CNN(nn.Module):
         hid_dim=32,
         norm_layer="batchnorm",
         activation="ReLU",
+        n_layers=None,
         **kwargs,
     ):
 
@@ -294,20 +299,28 @@ class CNN(nn.Module):
         self.hid_dim = hid_dim
         self.norm_layer = norm_layer
         self.activation = activation
+        self.n_layers = n_layers
+
+        # if want non pow2 you can pass it through a linear layer to get the closest power of 2
+        # and if transposed a linear layer to get in_shape. But better if it's done when image is
+        # smaller (or best is simply to resize it to power of 2)
+        assert is_pow2(self.in_shape[1]) and is_pow2(self.in_shape[2])
+
+        if self.n_layers is None:
+            # divide length by 2 at every step until smallest is 2
+            min_side = min(self.in_shape[1], self.in_shape[2])
+            self.n_layers = int(math.log2(min_side) - 1)
 
         Norm = get_Normalization(self.norm_layer, 2)
         # don't use bias with batch_norm https://twitter.com/karpathy/status/1013245864570073090?l...
         is_bias = Norm == nn.Identity
 
-        assert is_pow2(self.in_shape[1]) and is_pow2(self.in_shape[2])
-
-        n_layers = 4
         # for size 32 will go 32,16,8,4,2
         # channels for hid_dim=32: 3,32,64,128,256
         channels = [self.in_shape[0]]
-        channels += [self.hid_dim * (2 ** i) for i in range(0, n_layers)]
-        # by how much will increase after flattening
-        factor = (self.in_shape[1] * self.in_shape[2]) // ((2 ** n_layers) ** 2)
+        channels += [self.hid_dim * (2 ** i) for i in range(0, self.n_layers)]
+        end_h = self.in_shape[1] // (2 ** self.n_layers)
+        end_w = self.in_shape[2] // (2 ** self.n_layers)
 
         if self.is_transpose:
             channels.reverse()
@@ -323,13 +336,13 @@ class CNN(nn.Module):
 
         if self.is_transpose:
             layers = [
-                nn.Linear(self.out_dim, channels[0] * factor, bias=is_bias),
-                nn.Unflatten(dim=-1, unflattened_size=(channels[0], 2, 2)),
+                nn.Linear(self.out_dim, channels[0] * end_w * end_h, bias=is_bias),
+                nn.Unflatten(dim=-1, unflattened_size=(channels[0], end_h, end_w)),
             ] + layers
         else:
             layers += [
                 nn.Flatten(start_dim=1),
-                nn.Linear(channels[-1] * factor, self.out_dim),
+                nn.Linear(channels[-1] * end_w * end_h, self.out_dim),
                 # last layer should always have bias
             ]
 

lossyless/callbacks.py
@@ -13,7 +13,8 @@ import torchvision
 from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 
-from .helpers import BASE_LOG, plot_density, setup_grid, to_numpy, undo_normalization
+from .helpers import (BASE_LOG, UnNormalizer, is_colored_img, plot_density,
+                      setup_grid, to_numpy)
 
 try:
     import wandb
@@ -71,8 +72,12 @@ class ReconstructImages(Callback):
             #! waiting for torch lighning #1243
             x_hat = pl_module._save["Y_hat"].float()
             x = pl_module._save["X"].float()
-            # undo normalization for plotting
-            x_hat, x = undo_normalization(x_hat, x, pl_module.hparams.data.dataset)
+
+            if is_colored_img(x):
+                # undo normalization for plotting
+                unnormalizer = UnNormalizer(pl_module.hparams.data.dataset)
+                x = unnormalizer(x)
+
             caption = f"ep: {trainer.current_epoch}"
             save_img(pl_module, trainer, x_hat, "rec_img", caption)
             save_img(pl_module, trainer, x, "real_img", caption)
@@ -150,8 +155,8 @@ class LatentDimInterpolator(Callback):
         z = einops.rearrange(z, "r c ... -> (r c) ...")
         img = pl_module.distortion_estimator.q_YlZ(z)
 
-        # undo normalization for plotting
-        img, _ = undo_normalization(img, img, pl_module.hparams.data.dataset)
+        # put back to [0,1]
+        img = torch.sigmoid(img)
         return img
 
     def latent_traverse_2d(self, pl_module):

lossyless/helpers.py
@@ -254,64 +254,59 @@ STDS = dict(
 
 
 class Normalizer:
-    def __init__(self, dataset):
+    def __init__(self, dataset, is_raise=True):
         super().__init__()
+        self.dataset = dataset.lower()
         try:
             self.normalizer = transform_lib.Normalize(
-                mean=MEANS[dataset], std=STDS[dataset]
+                mean=MEANS[self.dataset], std=STDS[self.dataset]
             )
-        except:
-            self.normalizer = None
+        except KeyError:
+            if is_raise:
+                raise KeyError(
+                    f"dataset={self.dataset} wasn't found in MEANS={MEANS.keys()} or"
+                    f"STDS={STDS.keys()}. Please add mean and std."
+                )
+            else:
+                self.normalizer = None
 
     def __call__(self, x):
-        if x.size(-3) != 3 and self.normalizer is None:
-            # if not colored and wasn't in dict
+        if self.normalizer is None:
             return x
 
         return self.normalizer(x)
 
 
 class UnNormalizer:
-    def __init__(self, dataset):
+    def __init__(self, dataset, is_raise=True):
         super().__init__()
+        self.dataset = dataset.lower()
         try:
-            mean, std = MEANS[dataset], STDS[dataset]
+            mean, std = MEANS[self.dataset], STDS[self.dataset]
             self.unnormalizer = transform_lib.Normalize(
                 [-m / s for m, s in zip(mean, std)], std=[1 / s for s in std]
             )
-        except:
-            self.unnormalizer = None
+        except KeyError:
+            if is_raise:
+                raise KeyError(
+                    f"dataset={self.dataset} wasn't found in MEANS={MEANS.keys()} or"
+                    f"STDS={STDS.keys()}. Please add mean and std."
+                )
+            else:
+                self.normalizer = None
 
     def __call__(self, x):
-        if x.size(-3) != 3 and self.unnormalizer is None:
-            # if not colored and wasn't in dict
+        if self.unnormalizer is None:
             return x
 
         return self.unnormalizer(x)
 
 
-def get_normalization(Dataset):
-    """Return corrrect normalization given dataset class."""
-    if "cifar10" in Dataset.__name__.lower():
-        return Normalizer("cifar10")
-    elif "galaxy" in Dataset.__name__.lower():
-        return Normalizer("galaxy64")
-        # todo: different means for different resolution
-        # return Normalizer("galaxy129")
-    else:
-        raise ValueError(f"Uknown mean and std for {Dataset}.")
-
-
-def undo_normalization(Y_hat, targets, dataset):
-    """Undo transformation of predicted and target images given dataset name.
-    Used to ensure nice that can be used for plotting generated images."""
-
-    # images are in [0,1] due to `ToTensor` so can use sigmoid to ensure output is also in [0,1]
-    Y_hat = torch.sigmoid(Y_hat)
-
-    unnormalizer = UnNormalizer(dataset)
-
-    return unnormalizer(Y_hat), unnormalizer(targets)
+def is_colored_img(x):
+    """Check if an image or batch of image is colored."""
+    if x.shape[-3] not in [1, 3]:
+        raise ValueError(f"x doesn't seem to be a (batch of) image as shape={x.shape}.")
+    return x.shape[-3] == 3
 
 
 def atleast_ndim(x, ndim):

utils/data/images.py
@@ -9,10 +9,9 @@ from os import path
 
 import numpy as np
 import pandas as pd
-from PIL import Image
 
 import torch
-from lossyless.helpers import BASE_LOG, check_import, get_normalization
+from lossyless.helpers import BASE_LOG, Normalizer, check_import
 from torch.utils.data import random_split
 from torchvision import transforms as transform_lib
 from torchvision.datasets import CIFAR10, MNIST, FashionMNIST
@@ -69,17 +68,28 @@ class LossylessImgDataset(LossylessCLFDataset):
     def __init__(
         self, *args, equivalence={}, is_augment_val=False, is_normalize=True, **kwargs,
     ):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, is_normalize=is_normalize, **kwargs)
         self.equivalence = equivalence
         self.is_augment_val = is_augment_val
-        self.is_normalize = is_normalize
 
         self.base_tranform = self.get_base_transform()
         self.PIL_augment, self.tensor_augment = self.get_curr_augmentations()
 
+    @property
+    @abc.abstractmethod
+    def is_train(self):
+        """Whether considering training split."""
+        ...
+
     @abc.abstractmethod
     def get_img_target(self, index):
         """Return the unaugmented image (in PIL format) and target."""
+        ...
+
+    @property
+    @abc.abstractmethod
+    def dataset_name(self):
+        """Name of the dataset."""
         ...
 
     def get_x_target_Mx(self, index):
@@ -144,7 +154,8 @@ class LossylessImgDataset(LossylessCLFDataset):
 
         if self.is_normalize and self.is_color:
             # only normalize colored images
-            trnsfs += [get_normalization(type(self))]
+            # raise if can't normalize because you specifrically gave `is_normalize`
+            trnsfs += [Normalizer(self.dataset_name, is_raise=True)]
 
         return transform_lib.Compose(trnsfs)
 
@@ -163,7 +174,7 @@ class LossylessImgDataset(LossylessCLFDataset):
 
     def get_curr_augmentations(self):
         """Return the current augmentations transorms (tuple for PIL and tensor)."""
-        if self.is_augment_val or self.train:
+        if self.is_augment_val or self.is_train:
             PIL_augment, tensor_augment = self.get_augmentations()
             return PIL_augment, tensor_augment
         else:
@@ -202,6 +213,7 @@ class TorchvisionDataModule(LossylessDataModule):
             [len(dataset) - n_val, n_val],
             generator=torch.Generator().manual_seed(self.seed),
         )
+        valid.train = False
 
         return train, valid
 
@@ -252,6 +264,14 @@ class MnistDataset(LossylessImgDataset, MNIST):
         img, target = MNIST.__getitem__(self, index)
         return img, target
 
+    @property
+    def is_train(self):
+        return self.train
+
+    @property
+    def dataset_name(self):
+        return "MNIST"
+
 
 class MnistDataModule(TorchvisionDataModule):
     @property
@@ -273,6 +293,14 @@ class FashionMnistDataset(LossylessImgDataset, FashionMNIST):
         shapes["input"] = (1, 32, 32)
         shapes["target"] = (10,)
         return shapes
+
+    @property
+    def is_train(self):
+        return self.train
+
+    @property
+    def dataset_name(self):
+        return "FashionMNIST"
 
     processed_folder = MnistDataset.processed_folder
     raw_folder = MnistDataset.raw_folder
@@ -297,6 +325,14 @@ class Cifar10Dataset(LossylessImgDataset, CIFAR10):
         img, target = CIFAR10.__getitem__(self, index)
         return img, target
 
+    @property
+    def is_train(self):
+        return self.train
+
+    @property
+    def dataset_name(self):
+        return "CIFAR10"
+
 
 class Cifar10DataModule(TorchvisionDataModule):
     @property
@@ -320,18 +356,26 @@ class GalaxyDataset(LossylessImgDataset):
         resolution: int = 64,
         **kwargs,
     ):
+        # TODO check if need to normalize (if not add is_normalize=False in cfg) because very low std
         check_import("cv2", "GalaxyDataset")
 
         self.root = root
+        self.split = split
         data_dir = path.join(root, "galaxyzoo")
         self.resolution = resolution
-        if download:
+        if download and not self.is_exist_data(data_dir):
             self.download(data_dir)
             self.preprocess(data_dir)
 
         self.load_data(data_dir, split, resolution)
 
         super().__init__(*args, **kwargs)
+
+    def is_exist_data(self, data_dir):
+        # check if we already preprocessed and downloaded the data
+        # this is a hacky check, we just check if the last file that this
+        # routine yields exists
+        return path.exists(path.join(data_dir, "test_images_128.npy"))
 
     def download(self, data_dir):
         def unpack_all_zips():
@@ -359,25 +403,32 @@ class GalaxyDataset(LossylessImgDataset):
             )
             raise e
 
+        logger.info("Downloading Galaxy ...")
+
         # download the dataset
         bashCommand = (
             "kaggle competitions download -c "
             "galaxy-zoo-the-galaxy-challenge -p {}".format(self.root)
         )
-        subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        process = subprocess.Popen(
+            bashCommand.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        )
+        out = process.communicate()[0].decode("utf-8")
+        is_error = process.returncode != 0
+        if is_error:
+            logging.critical(
+                f"{bashCommand} failed with outputs: {out}. \n "
+                "Hint: don't forget to accept competition rules at "
+                "https://www.kaggle.com/c/galaxy-zoo-the-galaxy-challenge/rules. "
+            )
 
         # unpack the data
         with zipfile.ZipFile(os.path.join(self.root, filename), "r") as zip_ref:
-            zip_ref.extractall(self.root)
+            zip_ref.extractall(data_dir)
 
         unpack_all_zips()
 
     def preprocess(self, data_dir):
-        # check if we already preprocessed the data
-        # this is a hacky check, we just check if the last file that this
-        # routine yields exists
-        if path.exists(path.join(data_dir, "test_images_128.npy")):
-            return
 
         # SAVE IMAGE IDs
         def get_image_ids(image_dir):
@@ -471,8 +522,8 @@ class GalaxyDataset(LossylessImgDataset):
         return dict(
             PIL={
                 "rotation": RandomRotation(360),
-                "y_translation": RandomAffine(0, translate=(0, 0.5)),
-                "x_translation": RandomAffine(0, translate=(0.5, 0)),
+                "y_translation": RandomAffine(0, translate=(0, 0.25)),
+                "x_translation": RandomAffine(0, translate=(0.25, 0)),
                 "scale": RandomAffine(0, scale=(0.8, 1.2)),
                 "color": ColorJitter(
                     brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05
@@ -513,11 +564,20 @@ class GalaxyDataset(LossylessImgDataset):
 
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
-        img = Image.fromarray(img)
+        # equivalent: Image.fromarray((img.transpose(1,2,0)*255).astype(np.uint8))
+        img = transform_lib.functional.to_pil_image(torch.from_numpy(img), None)
 
         # don't apply transformation yet, it's done for you
 
         return img, target
+
+    @property
+    def is_train(self):
+        return self.split == "train"
+
+    @property
+    def dataset_name(self):
+        return f"galaxy{self.resolution}"
 
 
 class GalaxyDataModule(LossylessDataModule):

utils/estimators.py
@@ -97,7 +97,7 @@ def discrete_entropy(x, base=2, is_plugin=False, **kwargs):
     try:
         import ndd
     except ImportError:
-        logger.error(
+        logger.exception(
             "To compute discrete entropies you need to install `ndd`. Returning -inf."
         )
         return -np.inf