ad1fac215359fe403f02ecd679ba2adf74866def
YannDubs <yanndubois96@gmail.com>
2021-07-02 04:53:35 -0400

    [CLEARN] remove galaxy specific augmentations.

--- utils/data/images.py
+++ utils/data/images.py
@@ -13,7 +13,6 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import torch
-from lossyless.helpers import BASE_LOG, Normalizer, check_import, to_numpy
 from PIL import Image
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms as transform_lib
@@ -45,6 +44,8 @@ from torchvision.transforms import (
     ToTensor,
 )
 from tqdm import tqdm
+
+from lossyless.helpers import BASE_LOG, Normalizer, check_import, to_numpy
 from utils.helpers import remove_rf
 
 from .augmentations import (
@@ -288,9 +289,7 @@ class LossylessImgDataset(LossylessDataset):
                 "simclr_imagenet": get_simclr_augmentations("imagenet", shape[-1]),
                 "simclr_finetune": get_finetune_augmentations(shape[-1]),
             },
-            tensor={
-                "erasing": RandomErasing(value=0.5),
-            },
+            tensor={"erasing": RandomErasing(value=0.5),},
         )
 
     @property
@@ -452,10 +451,7 @@ class LossylessImgDataset(LossylessDataset):
 class LossylessImgDataModule(LossylessDataModule):
     def get_train_val_dataset(self, **dataset_kwargs):
         dataset = self.Dataset(
-            self.data_dir,
-            download=False,
-            curr_split="train",
-            **dataset_kwargs,
+            self.data_dir, download=False, curr_split="train", **dataset_kwargs,
         )
 
         n_val = int_or_ratio(self.val_size, len(dataset))
@@ -474,10 +470,7 @@ class LossylessImgDataModule(LossylessDataModule):
     def get_train_dataset(self, **dataset_kwargs):
         if "validation" in self.Dataset.get_available_splits():
             train = self.Dataset(
-                self.data_dir,
-                curr_split="train",
-                download=False,
-                **dataset_kwargs,
+                self.data_dir, curr_split="train", download=False, **dataset_kwargs,
             )
         else:
             # if there is no valdation split will compute it on the fly
@@ -499,10 +492,7 @@ class LossylessImgDataModule(LossylessDataModule):
 
     def get_test_dataset(self, **dataset_kwargs):
         test = self.Dataset(
-            self.data_dir,
-            curr_split="test",
-            download=False,
-            **dataset_kwargs,
+            self.data_dir, curr_split="test", download=False, **dataset_kwargs,
         )
         return test
 
@@ -747,12 +737,7 @@ class TensorflowBaseDataset(LossylessImgDataset, ImageFolder):
     min_size = 256
 
     def __init__(
-        self,
-        root,
-        curr_split="train",
-        download=True,
-        base_resize="clip",
-        **kwargs,
+        self, root, curr_split="train", download=True, base_resize="clip", **kwargs,
     ):
         check_import("tensorflow_datasets", "TensorflowBaseDataset")
 
@@ -1040,12 +1025,7 @@ class ExternalImgDataset(LossylessImgDataset):
     required_packages = []
 
     def __init__(
-        self,
-        root,
-        *args,
-        download=True,
-        curr_split="train",
-        **kwargs,
+        self, root, *args, download=True, curr_split="train", **kwargs,
     ):
         for p in self.required_packages:
             check_import(p, type(self).__name__)
@@ -1059,9 +1039,7 @@ class ExternalImgDataset(LossylessImgDataset):
         self.length = len(list(self.get_dir(curr_split).glob("*.jpeg")))
 
         super().__init__(
-            *args,
-            curr_split=curr_split,
-            **kwargs,
+            *args, curr_split=curr_split, **kwargs,
         )
 
     def get_dir(self, split=None):
@@ -1177,10 +1155,7 @@ class GalaxyDataset(ExternalImgDataset):
 
     required_packages = ["cv2", "kaggle"]
 
-    split_to_root = dict(
-        test="images_test_rev1",
-        train="images_training_rev1",
-    )
+    split_to_root = dict(test="images_test_rev1", train="images_training_rev1",)
 
     def __init__(
         self,
@@ -1202,9 +1177,7 @@ class GalaxyDataset(ExternalImgDataset):
             raise e
 
         super().__init__(
-            *args,
-            is_normalize=is_normalize,
-            **kwargs,
+            *args, is_normalize=is_normalize, **kwargs,
         )
 
     def download(self, data_dir):
@@ -1275,21 +1248,6 @@ class GalaxyDataset(ExternalImgDataset):
     def dataset_name(self):
         return "galaxy"
 
-    @property
-    def augmentations(self):
-        # TODO remove if we don't end up using those
-        augmentations = super().augmentations
-
-        # these are the augmentations used in kaggle
-        PIL_update = {
-            # in kaggle authors translate 69x69 images by /pm 4 pixel = 11.6%
-            "y_translation": RandomAffine(0, translate=(0, 0.116)),
-            "x_translation": RandomAffine(0, translate=(0.116, 0)),
-            "scale": RandomAffine(0, scale=(1.0 / 1.3, 1.3)),
-        }
-        augmentations["PIL"].update(PIL_update)
-        return augmentations
-
 
 class GalaxyDataModule(LossylessImgDataModule):
     @property
@@ -1316,25 +1274,17 @@ class CocoClipDataset(ExternalImgDataset):
     ]
 
     # test annotation are not given => use val instead
-    split_to_root = dict(
-        test="val2017",
-        train="train2017",
-    )
+    split_to_root = dict(test="val2017", train="train2017",)
     split_to_annotate = dict(
         test="annotations/captions_val2017.json",
         train="annotations/captions_train2017.json",
     )
 
     def __init__(
-        self,
-        *args,
-        base_resize="clip",
-        **kwargs,
+        self, *args, base_resize="clip", **kwargs,
     ):
         super().__init__(
-            *args,
-            base_resize=base_resize,
-            **kwargs,
+            *args, base_resize=base_resize, **kwargs,
         )
 
     def download(self, data_dir):
